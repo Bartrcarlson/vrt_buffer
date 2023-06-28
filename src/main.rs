5fbb04d1b6a38f99a90e85891d945c6f7ccc074c
Bart Carlson <bartcarlson.b.c@gmail.com>
2023-06-27 19:57:49 -0700

    crop works need to fix fn sigs

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,26 @@ pub fn vrt_buffer() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+pub fn buffer_down_to_size() -> Result<(), Box<dyn Error>> {
+    let out_crop_dir = PathBuf::from("output/cropped/");
+    // check if output directory exists and create it if not
+    fs::create_dir_all(&out_crop_dir)?;
+
+    // get a list of the buffered files
+    let paths = fs::read_dir(OUTPUT_DIR)?;
+    for path in paths {
+        let path = path?.path();
+        if let Some(extension) = path.extension().and_then(std::ffi::OsStr::to_str) {
+            if extension == "tif" || extension == "tiff" {
+                let input_path = Path::new(INPUT_DIR).join(path.file_name().unwrap());
+                let output_path = out_crop_dir.join(path.file_name().unwrap());
+                trim_buffered_to_size(&input_path, &path, &output_path)?;
+            }
+        }
+    }
+    Ok(())
+}
+
 fn add_margin_to_geotiff(
     file_path: &Path,
     output_path: &Path,
@@ -102,11 +122,25 @@ fn trim_buffered_to_size(
     output_raster: &PathBuf,
 ) -> Result<(), Box<dyn Error>> {
     let dso = Dataset::open(org_raster)?;
-    let dsb = Dataset::open(buffered_raster)?;
+    let mut dsb = Dataset::open(buffered_raster)?;
     let projo = dso.projection();
 
+    // Compute offsets in buffered raster based on geo_transform
+    let geo_transform_o = dso.geo_transform()?;
+    let geo_transform_b = dsb.geo_transform()?;
+    let x_offset = ((geo_transform_o[0] - geo_transform_b[0]) / geo_transform_b[1]) as usize;
+    let y_offset = ((geo_transform_o[3] - geo_transform_b[3]) / geo_transform_b[5]) as usize;
+
+    // read data from the buffered raster
+    let band = dsb.rasterband(1)?;
+    let buffered_data = band.read_as::<f64>(
+        (x_offset as isize, y_offset as isize),
+        (dso.raster_size().0, dso.raster_size().1),
+        (dso.raster_size().0, dso.raster_size().1),
+        None,
+    )?;
+
     // create an output raster with the same size as the original raster
-    // but the same data as the buffered raster
     let driver = DriverManager::get_driver_by_name("GTiff")?;
     let mut dso_out = driver.create_with_band_type::<f64, _>(
         output_raster.to_str().unwrap(),
@@ -114,11 +148,20 @@ fn trim_buffered_to_size(
         dso.raster_size().1 as isize,
         1,
     )?;
-    dso_out.set_geo_transform(&dso.geo_transform().unwrap())?;
+    dso_out.set_geo_transform(&geo_transform_o)?;
     dso_out.set_projection(&projo)?;
+
+    // write data to the output raster
+    let mut band_out = dso_out.rasterband(1)?;
+    band_out.write(
+        (0, 0),
+        (dso.raster_size().0, dso.raster_size().1),
+        &buffered_data,
+    )?;
 
     Ok(())
 }
 fn main() {
     vrt_buffer().unwrap();
+    buffer_down_to_size().unwrap();
 }
